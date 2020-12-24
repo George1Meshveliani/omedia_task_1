9a25f52a8053dd934b47d2dedd1f1eacb5b41890
George1Meshveliani <giorgi.meshveliani@gmail.com>
2020-12-25 02:42:22 +0400

    Configuration form.

--- modules/om_site_settings/src/Form/SiteSettingsForm.php
+++ modules/om_site_settings/src/Form/SiteSettingsForm.php
@@ -1,31 +1,113 @@
 <?php
 
+
 namespace Drupal\om_site_settings\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ConfigManager;
+use Drupal\Core\Config\ExtensionInstallStorage;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Class SiteSettingsForm.
+ *
+ * Copyright (C) 2020 Omedia <welcome@omedia.dev>.
+ *
+ * Omedia/OAS-API can not be copied and/or
+ * distributed without the express permission of Omedia.
+ *
+ * Written by Temuri Takalandze <takalandzet@gmail.com>, April 2020
+ *
+ * Class is used and modified by George Meshveliani member of Temuri Takalandze team.
+ *
+ * @package Drupal\om_site_settings\Form
  */
-class SiteSettingsForm extends FormBase {
+class SiteSettingsForm extends ConfigFormBase
+{
+
+  /**
+   * Drupal\Core\Config\ConfigManager definition.
+   *
+   * @var \Drupal\Core\Config\ConfigManager
+   */
+
+  protected $configManager;
+
+  /**
+   * Drupal\Core\Config\ExtensionInstallStorage definition.
+   *
+   * @var \Drupal\Core\Config\ExtensionInstallStorage
+   */
+  protected $configStorageSchema;
+
+  /**
+   * Constructs a new GeneralSettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   ConfigFactory Instance.
+   * @param \Drupal\Core\Config\ConfigManager $config_manager
+   *   ConfigManager Instance.
+   * @param \Drupal\Core\Config\ExtensionInstallStorage $config_storage_schema
+   *   ExtensionInstallStorage Instance.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    ConfigManager $config_manager,
+    ExtensionInstallStorage $config_storage_schema
+  )
+  {
+    parent::__construct($config_factory);
+    $this->configManager = $config_manager;
+    $this->configStorageSchema = $config_storage_schema;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public static function create(ContainerInterface $container)
+  {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('config.manager'),
+      $container->get('config.storage.schema')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames()
+  {
+    return [
+      'om_site_settings.site_settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function getFormId()
+  {
     return 'site_settings_form';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+
+    $config = $this->config('om_site_settings.site_settings');
+
     $form['archive_length'] = [
       '#type' => 'number',
       '#title' => $this->t('Archive Length'),
       '#description' => $this->t('Input number from 1 to 100'),
-      '#default_value' => '1',
+      '#default_value' => $config->get('archive_length'),
       '#weight' => '1',
       // Complete Validation - Archive Length
       '#min' => '1',
@@ -36,7 +118,7 @@ class SiteSettingsForm extends FormBase {
       '#title' => $this->t('Website Code'),
       '#description' => $this->t('Enter website code'),
       '#size' => 9,
-      '#default_value' => 'OM-00000',
+      '#default_value' => $config->get('website_code'),
       '#weight' => '1',
       // Partial Validation - Website Code
       '#maxlength' => 8,
@@ -45,7 +127,9 @@ class SiteSettingsForm extends FormBase {
     $form['website_description'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Website Description'),
-      '#description' => $this->t('Describe the website'),
+      '#description' => $this->t('Describe the website(Optional)'),
+      '#format' => $config->get('website_description')['format'] ?? 'full_html',
+      '#default_value' => $config->get('website_description')['value'] ?? '',
       '#weight' => '1',
     ];
     $form['submit'] = [
@@ -53,18 +137,19 @@ class SiteSettingsForm extends FormBase {
       '#value' => $this->t('Submit'),
     ];
 
-    return $form;
+    return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-     //  Fulfilled Validation - Website Code
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    //  Fulfilled Validation - Website Code
 
-      $website_code = $form_state->getValue('website_code');
+    $website_code = $form_state->getValue('website_code');
 
-    if (substr($website_code, 0, 3) !== "OM-")  {
+    if (substr($website_code, 0, 3) !== "OM-") {
       $form_state->setErrorByName('website_code', $this->t("Website Code should start with 'OM-' e.g: 'OM-12345'"));
     }
 
@@ -76,14 +161,27 @@ class SiteSettingsForm extends FormBase {
     parent::validateForm($form, $form_state);
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    parent::submitForm($form, $form_state);
+
+    $fields = [
+      'archive_length',
+      'website_code',
+      'description',
+    ];
+
+    foreach ($fields as $field_key) {
+      $this->config('om_site_settings.site_settings')
+        ->set($field_key, $form_state->getValue($field_key))
+        ->save();
+
+    }
+
+    foreach ($form_state->getValues($field_key) as $key => $value) {
       \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
     }
-  }
 
+  }
 }
+
